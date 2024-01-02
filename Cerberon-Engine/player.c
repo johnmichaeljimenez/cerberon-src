818c47c3b81706fc07088574fc121b3cc49a6f82
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-03 00:32:59 +0800

    WIP player character implementation

--- Cerberon-Engine/player.c
+++ Cerberon-Engine/player.c
@@ -2,3 +2,38 @@
 #include <raymath.h>
 #include "asset_manager.h"
 #include "player.h"
+
+void PlayerInit(PlayerCharacter* p)
+{
+	p->Position = Vector2Zero();
+	p->Rotation = 0;
+	p->Direction = (Vector2){ 1, 0 };
+	p->CollisionRadius = 28;
+	p->MovementSpeed = 200;
+}
+
+void PlayerUnload(PlayerCharacter* p)
+{
+
+}
+
+void PlayerUpdate(PlayerCharacter* p)
+{
+
+}
+
+void PlayerLateUpdate(PlayerCharacter* p)
+{
+
+}
+
+void PlayerDraw(PlayerCharacter* p)
+{
+	DrawCircleLines(p->Position.x, p->Position.y, p->CollisionRadius, GREEN);
+	DrawLineV(p->Position, Vector2Add(p->Position, Vector2Scale(p->Direction, p->CollisionRadius)), GREEN);
+}
+
+void PlayerDrawHUD(PlayerCharacter* p)
+{
+
+}
--- Cerberon-Engine/player.h
+++ Cerberon-Engine/player.h
@@ -2,3 +2,23 @@
 #include <raylib.h>
 #include <raymath.h>
 #include "asset_manager.h"
+
+typedef struct PlayerCharacter
+{
+	Vector2 Position;
+	float Rotation;
+	Vector2 Direction;
+
+	float MovementSpeed;
+	float CollisionRadius;
+
+} PlayerCharacter;
+
+PlayerCharacter PlayerEntity;
+
+void PlayerInit(PlayerCharacter* p);
+void PlayerUnload(PlayerCharacter* p);
+void PlayerUpdate(PlayerCharacter* p);
+void PlayerLateUpdate(PlayerCharacter* p);
+void PlayerDraw(PlayerCharacter* p);
+void PlayerDrawHUD(PlayerCharacter* p);
