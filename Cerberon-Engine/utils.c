523001305a47b5a1d239e70136b64f7aaa4ff9f8
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-04 00:14:24 +0800

    Added DrawSprite for centered sprite drawing

--- Cerberon-Engine/utils.c
+++ Cerberon-Engine/utils.c
@@ -2,12 +2,21 @@
 #include "utils.h"
 
 unsigned long ToHash(unsigned char* str) {
-    unsigned long hash = 5381;
-    int c;
+	unsigned long hash = 5381;
+	int c;
 
-    while (c = *str++) {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
+	while (c = *str++) {
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
 
-    return hash;
+	return hash;
+}
+
+void DrawSprite(TextureResource* t, Vector2 pos, float rotation, float scale)
+{
+	Rectangle src = (Rectangle){ 0, 0, t->Texture.width, t->Texture.height };
+	Rectangle dest = (Rectangle){ pos.x, pos.y, t->Texture.width * scale, t->Texture.height * scale };
+	Vector2 origin = (Vector2){ dest.width / 2, dest.height / 2 };
+
+	DrawTexturePro(t->Texture, src, dest, origin, rotation * RAD2DEG, WHITE);
 }
--- Cerberon-Engine/utils.h
+++ Cerberon-Engine/utils.h
@@ -1,4 +1,6 @@
 #pragma once
 #include <raylib.h>
+#include "asset_manager.h"
 
 unsigned long ToHash(unsigned char* str);
+void DrawSprite(TextureResource* t, Vector2 pos, float rotation, float scale);
