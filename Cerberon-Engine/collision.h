affff361ed5b60c503b3c54d5a79f614abfb7d7c
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-04 01:57:23 +0800

    Added character body collision system

--- Cerberon-Engine/collision.h
+++ Cerberon-Engine/collision.h
@@ -4,3 +4,6 @@
 #include "mapdata_manager.h"
 
 void MoveBody(Vector2* pos, float radius);
+bool GetLineIntersection(float p0_x, float p0_y, float p1_x, float p1_y,
+	float p2_x, float p2_y, float p3_x, float p3_y, Vector2* hit);
+Vector2 WallGetClosestPoint(Vector2 p1, Vector2 p2, Vector2 pos);
