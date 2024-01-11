10f0b64f7d93108833d3d307f60012ed609affa0
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-12 00:48:40 +0800

    WIP asset compiler setup

--- Cerberon-Exporter/main.c
+++ Cerberon-Exporter/main.c
@@ -2,7 +2,45 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int c, char* a)
+int export(char* directory);
+
+int main(int c, char** a)
 {
+	if (c != 2)
+	{
+		printf("Directory is empty!\n");
+		getch();
+		return 0;
+	}
+
+	char* directory = a[1];
+	return export(directory);
+}
+
+
+int export(char* directory)
+{
+	if (!DirectoryExists(directory))
+	{
+		printf("Directory not found: %s", directory);
+		return 1;
+	}
+
+	char* spritesDir = TextFormat("%s\\sprites\\\n", directory);
+	char* tilesDir = TextFormat("%s\\tiles\\\n", directory);
+	printf(spritesDir);
+	printf(tilesDir);
+
+	if (!DirectoryExists(spritesDir))
+	{
+
+	}
+
+	if (!DirectoryExists(tilesDir))
+	{
+
+	}
+
+	getch();
 	return 0;
 }
