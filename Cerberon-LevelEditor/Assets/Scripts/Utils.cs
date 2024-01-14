ee58b851d4c4ab140cd01797a934c604034d20a5
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-14 18:06:24 +0800

    Fixed level editor string exporter

--- Cerberon-LevelEditor/Assets/Scripts/Utils.cs
+++ Cerberon-LevelEditor/Assets/Scripts/Utils.cs
@@ -2,10 +2,11 @@ public static class Utils
 {
     public static string ToFixedLength(this string str, int fixedLength)
     {
-        fixedLength -= 1; //reserve last index to null terminator
-        if (str.Length > fixedLength)
-            return str.Substring(0, fixedLength) + "\0";
+        if (str.Length >= fixedLength)
+            str = str.Substring(0, fixedLength-1) + "\0";
         else
-            return str.PadRight(fixedLength, ' ') + "\0"; //TODO: Insert null terminator on last character before padding
+            str = (str + "\0").PadRight(fixedLength, ' ');
+
+        return str;
     }
 }
