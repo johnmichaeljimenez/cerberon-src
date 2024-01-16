7ac9c2d203f5d6ddf79e1d74ece1cf9b4685e8bd
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-17 01:12:12 +0800

    Trigger struct adjustments

--- Cerberon-Engine/i_trigger.h
+++ Cerberon-Engine/i_trigger.h
@@ -1,15 +1,22 @@
 #pragma once
 #include <raylib.h>
 
-typedef struct Trigger
+typedef struct TriggerCollider
 {
 	Vector2 Position;
 	float Rotation;
 	Vector2 Size;
+} TriggerCollider;
 
+typedef struct Trigger
+{
 	char* Target[32];
-	float Delay;
 	float OneShot;
 	float Cooldown;
 
+	int ColliderCount;
+	TriggerCollider* Colliders;
+
+	bool _activated;
+	float _timer;
 } Trigger;
