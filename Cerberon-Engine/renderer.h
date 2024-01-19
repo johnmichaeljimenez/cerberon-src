b06eb0b9adc23d7aa1a199784b9fbfe844769512
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-20 02:44:15 +0800

    Added Rendering system data structures

--- Cerberon-Engine/renderer.h
+++ Cerberon-Engine/renderer.h
@@ -1,9 +1,31 @@
 #pragma once
 #include <raylib.h>
 
+typedef enum RenderLayer
+{
+	RENDERLAYER_Ground,
+	RENDERLAYER_Entity,
+	RENDERLAYER_Wall,
+	RENDERLAYER_Light,
+	RENDERLAYER_Overlay,
+} RenderLayer;
+
+typedef struct RenderObject
+{
+	RenderLayer Layer;
+	int SortingIndex;
+	Rectangle Bounds;
+	void* Data;
+	void(*OnDraw)(void*);
+} RenderObject;
+
+int RenderObjectCount;
+RenderObject* RenderObjectList;
+
 RenderTexture2D RendererScreenTexture;
 RenderTexture2D RendererEffectsTexture;
 
+void CreateRenderObject(RenderLayer renderLayer, int sortingIndex, Rectangle bounds, void* data, void(*onDraw)(void*));
 void RendererInit();
 void RendererUnload();
 void RendererDraw();
