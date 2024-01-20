f24c7e2d2383809edef16e1db11bc6d96e2ede4c
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-21 00:46:24 +0800

    Added FSM functions and state flag

--- Cerberon-Engine/fsm.c
+++ Cerberon-Engine/fsm.c
@@ -1,1 +1,19 @@
 #include "fsm.h"
+#include <stdlib.h>
+
+FSMState FSMStateCreate(FSM* fsm, char* name, void* data, FSMStateFlags flags,
+	void(*onInit)(FSMState* s),
+	void(*onEnter)(FSMState* s),
+	void(*onUpdate)(FSMState* s),
+	void(*onExit)(FSMState* s)
+)
+{
+
+}
+
+
+void FSMUpdate(FSM* fsm)
+{
+	if (fsm->CurrentState != NULL)
+		fsm->CurrentState->OnUpdate(fsm->CurrentState);
+}
--- Cerberon-Engine/fsm.h
+++ Cerberon-Engine/fsm.h
@@ -1,9 +1,20 @@
 #pragma once
+#include <stdbool.h>
+
+typedef enum FSMStateFlags
+{
+	FSMFlag_None = 1 << 0,
+	FSMFlag_Physics = 1 << 1,
+	FSMFlag_CanAttack = 1 << 2,
+	FSMFlag_CannotBeInterrupted = 1 << 3,
+	FSMFlag_DisableMovement = 1 << 4
+} FSMStateFlags;
 
 typedef struct FSMState
 {
-	FSM* FSM;
+	struct FSM* FSM;
 	bool IsValid;
+	FSMStateFlags Flags;
 	char* Name[32];
 	unsigned long Hash;
 
@@ -21,3 +32,12 @@ typedef struct FSM
 	FSMState States[16];
 	FSMState* CurrentState;
 } FSM;
+
+FSMState FSMStateCreate(FSM* fsm, char* name, void* data, FSMStateFlags flags,
+	void(*onInit)(FSMState* s),
+	void(*onEnter)(FSMState* s),
+	void(*onUpdate)(FSMState* s),
+	void(*onExit)(FSMState* s)
+);
+
+void FSMUpdate(FSM* fsm);
