08c527d30c5f1a3673c5e09067d4bc4137d8ce5f
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-25 01:58:34 +0800

    Added time of day status

--- Cerberon-Engine/time.c
+++ Cerberon-Engine/time.c
@@ -3,7 +3,10 @@
 #include "utils.h"
 #include <raymath.h>
 
-float MaxTimePerDay = 120.0f;
+static float MaxTimePerDay = 30.0f;
+static float MorningTime = 0.25f;
+static float EveningTime = 0.85f;
+
 static int _currentTimeIndex;
 static Color _previousAmbientColor;
 static Color _currentAmbientColor;
@@ -19,11 +22,12 @@ void TimeInit()
 
 void TimeUpdate()
 {
+	float d = GetCurrentTimeOfDay();
 	CurrentTimeOfDay += TICKRATE;
 	if (CurrentTimeOfDay >= MaxTimePerDay)
 		CurrentTimeOfDay = 0;
 
-	_currentTimeIndex = (int)floorf(GetCurrentTimeOfDay() * 16);
+	_currentTimeIndex = (int)floorf(d * 16);
 
 	lerpColorAmount += TICKRATE;
 	if (lerpColorAmount >= maxSegment)
@@ -32,6 +36,15 @@ void TimeUpdate()
 		_previousAmbientColor = _currentAmbientColor;
 		_currentAmbientColor = TimeOfDayGradient[_currentTimeIndex];
 	}
+
+	if (d < MorningTime)
+		CurrentTimeStatus = TIMESTATUS_Midnight;
+	else if (d < 0.5f)
+		CurrentTimeStatus = TIMESTATUS_Morning;
+	else if (d < EveningTime)
+		CurrentTimeStatus = TIMESTATUS_Afternoon;
+	else
+		CurrentTimeStatus = TIMESTATUS_Evening;
 }
 
 float GetCurrentTimeOfDay()
@@ -57,5 +70,5 @@ void DrawDebugTime()
 	DrawRectangle(4, 100, 32, 32, _previousAmbientColor);
 	DrawRectangle(4, 140, 32, 32, _currentAmbientColor);
 
-	DrawText(TextFormat("%.2f/%.2f = %.2f ---> %.2f %.2f", CurrentTimeOfDay, MaxTimePerDay, GetCurrentTimeOfDay(), lerpColorAmount, maxSegment), 4, 50, 32, GREEN);
+	DrawText(TextFormat("%d %.2f/%.2f = %.2f ---> %.2f %.2f", CurrentTimeStatus, CurrentTimeOfDay, MaxTimePerDay, GetCurrentTimeOfDay(), lerpColorAmount, maxSegment), 4, 50, 32, GREEN);
 }
--- Cerberon-Engine/time.h
+++ Cerberon-Engine/time.h
@@ -5,9 +5,18 @@
 #pragma once
 #include <raylib.h>
 
+typedef enum TimeStatus
+{
+	TIMESTATUS_Midnight,
+	TIMESTATUS_Morning,
+	TIMESTATUS_Afternoon,
+	TIMESTATUS_Evening
+} TimeStatus;
+
 float TICKRATE;
 float CurrentTimeOfDay;
 Color TimeOfDayGradient[16];
+TimeStatus CurrentTimeStatus;
 
 void TimeInit();
 void TimeUpdate();
