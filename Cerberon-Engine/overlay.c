d8bd8fd0c6e3d0548e841c844dad465c4a269145
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-26 01:41:13 +0800

    Overlay init and draw implementation

--- Cerberon-Engine/overlay.c
+++ Cerberon-Engine/overlay.c
@@ -1,14 +1,25 @@
 #include <raylib.h>
 #include "overlay.h"
 #include "mapdata_manager.h"
-
+#include "asset_manager.h"
+#include "utils.h"
 
 void OverlayInit()
 {
+	for (int i = 0; i < CurrentMapData->OverlayCount; i++)
+	{
+		Overlay* o = &CurrentMapData->Overlays[i];
 
+		o->_textureResource = GetTextureResource(ToHash(o->TextureID));
+		//TODO: Calculate bounding box from rotated rectangle;
+	}
 }
 
 void OverlayDraw()
 {
-
+	for (int i = 0; i < CurrentMapData->OverlayCount; i++)
+	{
+		Overlay* o = &CurrentMapData->Overlays[i];
+		DrawSprite(o->_textureResource, o->Position, o->Rotation, o->Scale.x, Vector2Zero(), BLACK);
+	}
 }
