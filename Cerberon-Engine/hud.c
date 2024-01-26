88876b0642cfde7ae89904f2cb6b1650cd86f34f
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-27 03:52:40 +0800

    Added player inventory HUD drawing

--- Cerberon-Engine/hud.c
+++ Cerberon-Engine/hud.c
@@ -14,6 +14,19 @@ void DrawHUD()
 	DrawRectangle(4, 4, (char)(120.0 * (PlayerEntity.Hitpoints / 100.0)), 15, (Color){120, 0, 0, 255 });
 	DrawRectangleLines(4, 4, 120, 15, WHITE);
 
+	//inventory
+	for (int i = 0; i < InventoryMaxSize; i++)
+	{
+		float y = 24 + (i * 32) + (i * 4);
+
+		ItemPickup* in = &InventoryPlayer.Items[i];
+		DrawRectangleLines(4, y, 32, 32, in->IsActive? WHITE : DARKGRAY);
+		if (!in->IsActive)
+			continue;
+
+		DrawText(in->CurrentMaxAmount > 1 ? TextFormat("%d/%d", in->CurrentAmount, in->CurrentMaxAmount) : TextFormat("%d", in->CurrentAmount), 6, y + 2, 12, WHITE);
+	}
+
 	//misc
 	DrawText(TimeGetString(), 4, size.y - 20, 12, WHITE);
 }
