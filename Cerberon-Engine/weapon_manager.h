03c89f8bbfe86ed7537f8482bd726924c8aa3235
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-28 16:14:20 +0800

    Weapon data and initialization setup

--- Cerberon-Engine/weapon_manager.h
+++ Cerberon-Engine/weapon_manager.h
@@ -6,19 +6,23 @@
 
 typedef struct Weapon
 {
-	ItemPickup* itemData;
+	ItemPickup* itemReference;
 	int CurrentAmmo1, CurrentAmmo2;
 
 	char* Name[32];
+	bool IsMelee;
 	int MaxAmmo1, MaxAmmo2;
 	void(*OnInit)(struct Weapon* w);
 	void(*OnFire)(struct Weapon* w);
 	void(*OnReload)(struct Weapon* w);
 
+	bool _isValid;
 	bool _isActive;
 	float _timer;
 } Weapon;
 
+Weapon weaponData[32];
+
 void WeaponInitData();
-void WeaponLoad(Weapon* w);
+Weapon WeaponGive(Weapon* refWeapon, ItemPickup* refItem, int ammo1, int ammo2);
 void WeaponUpdate(Weapon* w);
