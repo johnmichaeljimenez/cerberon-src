4301dda5250d5ce9cabd03574ddea40b9ec46166
John <johnmichaeljimenez.thecmd@gmail.com>
2024-02-03 02:01:23 +0800

    Added UI Panel and UI Control draw and update functions

--- Cerberon-Engine/ui_manager.c
+++ Cerberon-Engine/ui_manager.c
@@ -31,20 +31,49 @@ void UISetSelection(UIControl* c)
 
 void UIUpdate()
 {
+	UIControl* newHovered;
 
+	for (int i = 0; i < 32; i++)
+	{
+		UIPanel* p = &UIPanelList[i];
+		if (!p->IsValid || !p->IsVisible)
+			continue;
+	}
 }
 
 void UIDraw()
 {
+	for (int i = 0; i < 32; i++)
+	{
+		UIPanel* p = &UIPanelList[i];
+		if (!p->IsValid || !p->IsVisible)
+			continue;
 
+		for (int j = 0; j < 32; j++)
+		{
+			UIControl* c = p->Controls[j];
+			if (!c->IsValid || !c->IsVisible)
+				continue;
+
+			c->OnDraw(c);
+		}
+	}
 }
 
 void UIShow(UIPanel* c)
 {
+	UIIsVisible = true;
+	c->IsVisible = true;
 
+	if (c->OnShow != NULL)
+		c->OnShow(c);
 }
 
 void UIHide(UIPanel* c)
 {
+	UIIsVisible = false;
+	c->IsVisible = false;
 
+	if (c->OnHide != NULL)
+		c->OnHide(c);
 }
--- Cerberon-Engine/ui_manager.h
+++ Cerberon-Engine/ui_manager.h
@@ -4,6 +4,8 @@ typedef struct UIControl
 {
 	Rectangle Rect;
 
+	bool IsVisible;
+	bool IsValid;
 	bool Clickable;
 	bool Selected;
 	struct UIPanel* Parent;
@@ -11,18 +13,20 @@ typedef struct UIControl
 	void(*OnDeselect)(struct UIControl* c);
 	void(*OnSelect)(struct UIControl* c);
 	void(*OnClick)(struct UIControl* c);
-	void(*OnDraw)(void* data);
+	void(*OnDraw)(struct UIControl* c);
 } UIControl;
 
 typedef struct UIPanel
 {
 	UIControl* Controls[32];
-	bool Visible;
+	bool IsVisible;
+	bool IsValid;
 
 	void (*OnShow)(UIControl* c);
 	void (*OnHide)(UIControl* c);
 } UIPanel;
 
+UIPanel* UICurrentPanel;
 UIPanel UIPanelList[32];
 UIControl* UICurrentSelected;
 UIControl* UICurrentHovered;
