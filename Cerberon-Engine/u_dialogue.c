bbaccf511567d9405c4b1489c9495696ae613c75
John <johnmichaeljimenez.thecmd@gmail.com>
2024-02-03 22:28:08 +0800

    Added u_dialogue UI Panel

---
+++ Cerberon-Engine/u_dialogue.c
@@ -0,0 +1,28 @@
+#include <raylib.h>
+#include "ui_manager.h"
+#include "u_dialogue.h"
+
+void UDialogueInit(UIPanel* u)
+{
+
+}
+
+void UDialogueUpdate(UIPanel* u)
+{
+
+}
+
+void UDialogueShow(UIPanel* u)
+{
+
+}
+
+void UDialogueHide(UIPanel* u)
+{
+
+}
+
+void UDialogueDraw(UIPanel* u)
+{
+
+}
---
+++ Cerberon-Engine/u_dialogue.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "ui_manager.h"
+
+void UDialogueInit(UIPanel* u);
+void UDialogueUpdate(UIPanel* u);
+void UDialogueShow(UIPanel* u);
+void UDialogueHide(UIPanel* u);
+void UDialogueDraw(UIPanel* u);
