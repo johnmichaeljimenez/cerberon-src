420a69c4ab4f0b052f8127547352366d2ffffcfa
John <johnmichaeljimenez.thecmd@gmail.com>
2024-02-03 23:11:27 +0800

    Added Dialogue test layout

--- Cerberon-Engine/u_dialogue.c
+++ Cerberon-Engine/u_dialogue.c
@@ -24,5 +24,6 @@ void UDialogueHide(UIPanel* u)
 
 void UDialogueDraw(UIPanel* u)
 {
-
+	DrawRectangle(12, GetScreenHeight() - 212, GetScreenWidth() - 24, 200, (Color){255,255,255,50});
+	DrawText("hello", 24, GetScreenHeight() - 200, 25, WHITE);
 }
