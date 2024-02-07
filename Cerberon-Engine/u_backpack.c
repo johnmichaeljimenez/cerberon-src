15ebea04fb24b97fd9d0bfa9efa5c740ce7b3c7c
John <johnmichaeljimenez.thecmd@gmail.com>
2024-02-07 23:26:21 +0800

    Added Backpack UI

---
+++ Cerberon-Engine/u_backpack.c
@@ -0,0 +1,20 @@
+#include <raylib.h>
+#include "ui_manager.h"
+#include "inventory.h"
+#include "player.h"
+#include "u_backpack.h"
+
+void UBackpackCreate(UIElement* e)
+{
+
+}
+
+void UBackpackShow(UIElement* e)
+{
+
+}
+
+void UBackpackDraw(UIElement* e)
+{
+
+}
---
+++ Cerberon-Engine/u_backpack.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <raylib.h>
+#include "ui_manager.h"
+#include "inventory.h"
+#include "player.h"
+
+void UBackpackCreate(UIElement* e);
+void UBackpackShow(UIElement* e);
+void UBackpackDraw(UIElement* e);
