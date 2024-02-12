09e2e566331dd1f1c962d0fde7391e214657d7db
John <johnmichaeljimenez.thecmd@gmail.com>
2024-02-13 01:24:47 +0800

    Added Prop struct

---
+++ Cerberon-Engine/prop.c
@@ -0,0 +1,2 @@
+#include <raylib.h>
+#include "prop.h"
---
+++ Cerberon-Engine/prop.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <raylib.h>
+
+typedef enum PropComponentType
+{
+	PROPCOMPONENTTYPE_Sprite,
+	PROPCOMPONENTTYPE_SpriteTiled,
+	PROPCOMPONENTTYPE_BoxCollider,
+	PROPCOMPONENTTYPE_CircleCollider,
+	PROPCOMPONENTTYPE_EdgeCollider,
+	PROPCOMPONENTTYPE_Light
+} PropComponentType;
+
+typedef struct PropComponent
+{
+	PropComponentType Type;
+	void* Data;
+	void(*OnInit)(struct Prop* p, struct PropComponent* c);
+};
+
+typedef struct Prop
+{
+	Vector2 Position;
+	float Rotation;
+	char ID[16];
+	struct PropComponent Components[16];
+} Prop;
