0c269ffce2d8c4b25e6a1c11003b55630832396d
John <johnmichaeljimenez.thecmd@gmail.com>
2024-02-18 19:19:02 +0800

    Added PrefabExporter tool in Level Editor

---
+++ Cerberon-LevelEditor/Assets/Editor/PrefabExporter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class PrefabExporter : MonoBehaviour
+{
+    private const string prefabDir = "Prefabs/Props";
+
+    [MenuItem("Tools/Export PREFAB file")]
+    static void DoSomething()
+    {
+        var prefabs = AssetDatabase.FindAssets("t:prefab", new string[] { $"Assets/{prefabDir}" });
+        foreach (var item in prefabs)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(item);
+            var g = AssetDatabase.LoadAssetAtPath(path, typeof(GameObject));
+
+
+        }
+    }
+}
