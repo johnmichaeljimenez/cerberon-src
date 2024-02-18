84322ad23db8e5d7776aa6ba1bd3ff48562be2db
John <johnmichaeljimenez.thecmd@gmail.com>
2024-02-18 21:06:55 +0800

    Added Prefab Exporter file saving

--- Cerberon-LevelEditor/Assets/Editor/PrefabExporter.cs
+++ Cerberon-LevelEditor/Assets/Editor/PrefabExporter.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -12,13 +14,40 @@ public class PrefabExporter : MonoBehaviour
     [MenuItem("Tools/Export PREFAB file")]
     static void DoSomething()
     {
+        var fname = EditorUtility.SaveFilePanel("Export to", "", $"prefab.data", "dat");
+        var objects = new Dictionary<GameObject, List<Component>>();
+
+        if (string.IsNullOrEmpty(fname))
+            return;
+
+        var data = new List<byte>();
         var prefabs = AssetDatabase.FindAssets("t:prefab", new string[] { $"Assets/{prefabDir}" });
         foreach (var item in prefabs)
         {
             var path = AssetDatabase.GUIDToAssetPath(item);
-            var g = AssetDatabase.LoadAssetAtPath(path, typeof(GameObject));
+            var g = AssetDatabase.LoadAssetAtPath(path, typeof(GameObject)) as GameObject;
 
+            objects.Add(g, new List<Component>());
+
+            foreach (var j in g.GetComponentsInChildren<Component>())
+            {
+                var valid = false;
+
+                if (!valid)
+                    continue;
+
+                objects[g].Add(j);
+            }
+        }
+        objects = objects.Where(p => p.Value.Count > 0).ToDictionary(p => p.Key, p => p.Value);
+
+        foreach (var obj in objects)
+        {
 
         }
+
+
+        File.WriteAllBytes(fname, data.ToArray());
+        EditorUtility.DisplayDialog("Success!", $"Prefab data exported to {fname}", "OK");
     }
 }
