0febedd2e8562d64f09b89074ef971e6f936c5a5
John <johnmichaeljimenez.thecmd@gmail.com>
2024-02-18 22:59:07 +0800

    WIP Prefab Exporter data writer

--- Cerberon-LevelEditor/Assets/Editor/PrefabExporter.cs
+++ Cerberon-LevelEditor/Assets/Editor/PrefabExporter.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using Unity.Plastic.Newtonsoft.Json.Linq;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -14,11 +16,15 @@ public class PrefabExporter : MonoBehaviour
     [MenuItem("Tools/Export PREFAB file")]
     static void DoSomething()
     {
-        var fname = EditorUtility.SaveFilePanel("Export to", "", $"prefab.data", "dat");
+        var fname = EditorUtility.SaveFilePanel("Export to", "", $"prefab", "dat");
         var objects = new Dictionary<GameObject, List<Component>>();
 
         if (string.IsNullOrEmpty(fname))
             return;
+
+        var types = new Type[]{
+            typeof(SpriteRenderer), typeof(BoxCollider2D), typeof(CircleCollider2D)
+        };
 
         var data = new List<byte>();
         var prefabs = AssetDatabase.FindAssets("t:prefab", new string[] { $"Assets/{prefabDir}" });
@@ -31,7 +37,7 @@ public class PrefabExporter : MonoBehaviour
 
             foreach (var j in g.GetComponentsInChildren<Component>())
             {
-                var valid = false;
+                var valid = types.Contains(j.GetType());
 
                 if (!valid)
                     continue;
@@ -39,15 +45,35 @@ public class PrefabExporter : MonoBehaviour
                 objects[g].Add(j);
             }
         }
-        objects = objects.Where(p => p.Value.Count > 0).ToDictionary(p => p.Key, p => p.Value);
+        //objects = objects.Where(p => p.Value.Count > 0).ToDictionary(p => p.Key, p => p.Value);
+
+
+        //object count
+        data.AddRange(BitConverter.GetBytes(objects.Count));
 
         foreach (var obj in objects)
         {
+            //object name
+            data.AddRange(Encoding.ASCII.GetBytes(obj.Key.name.ToFixedLength(32)));
 
+            //component count
+            data.AddRange(BitConverter.GetBytes(obj.Value.Count));
+            if (obj.Value.Count > 0)
+                continue;
+
+            foreach (var item in obj.Value)
+            {
+                WriteComponent(item, data);
+            }
         }
 
 
         File.WriteAllBytes(fname, data.ToArray());
         EditorUtility.DisplayDialog("Success!", $"Prefab data exported to {fname}", "OK");
+    }
+
+    private static void WriteComponent(Component c, List<byte> data)
+    {
+
     }
 }
