d9694f173264675a58f6d751ef9b7e7a788886ef
John <johnmichaeljimenez.thecmd@gmail.com>
2024-03-06 01:25:21 +0800

    Added Basic Protobuf serializer with GZip

--- Cerberon-LevelEditor/Assets/Editor/MapExporter.cs
+++ Cerberon-LevelEditor/Assets/Editor/MapExporter.cs
@@ -5,6 +5,10 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using System.IO;
 using System;
+using ProtoBuf;
+using ProtoBuf.Meta;
+using static MapExporter;
+using System.IO.Compression;
 
 public class MapExporter : MonoBehaviour
 {
@@ -37,19 +41,35 @@ public class MapExporter : MonoBehaviour
         if (string.IsNullOrEmpty(fname))
             return;
 
-        var data = new List<byte>();
-        data.AddRange(BitConverter.GetBytes(player.position.x * MAP_SCALE));
-        data.AddRange(BitConverter.GetBytes(player.position.y * MAP_SCALE_Y));
-        data.AddRange(BitConverter.GetBytes(player.eulerAngles.z));
+        var model = RuntimeTypeModel.Default;
+        model.Add<Vector2Surrogate>();
+        model.Add<Vector2>(false).SetSurrogate(typeof(Vector2Surrogate));
 
-        Export<BaseWall>(root, data);
-        Export<BaseInteractable>(root, data);
-        Export<LightObject>(root, data);
-        Export<TileObject>(root, data);
-        Export<TriggerObject>(root, data);
-        Export<OverlayObject>(root, data);
+        var mapData = new MapData();
+        mapData.PlayerPosition = new Vector2(player.position.x * MAP_SCALE, player.position.y * MAP_SCALE_Y);
+        mapData.PlayerRotation = player.eulerAngles.z;
 
-        File.WriteAllBytes(fname, data.ToArray());
+        using (var i = new FileStream(fname, FileMode.OpenOrCreate))
+        {
+            using (var gzip = new GZipStream(i, CompressionMode.Compress))
+            {
+                Serializer.Serialize(gzip, mapData);
+            }
+        }
+
+        //var data = new List<byte>();
+        //data.AddRange(BitConverter.GetBytes(player.position.x * MAP_SCALE));
+        //data.AddRange(BitConverter.GetBytes(player.position.y * MAP_SCALE_Y));
+        //data.AddRange(BitConverter.GetBytes(player.eulerAngles.z));
+
+        //Export<BaseWall>(root, data);
+        //Export<BaseInteractable>(root, data);
+        //Export<LightObject>(root, data);
+        //Export<TileObject>(root, data);
+        //Export<TriggerObject>(root, data);
+        //Export<OverlayObject>(root, data);
+
+        //File.WriteAllBytes(fname, data.ToArray());
         EditorUtility.DisplayDialog("Success!", $"Map exported to {fname}", "OK");
     }
 
@@ -62,5 +82,33 @@ public class MapExporter : MonoBehaviour
         {
             i.Export(array);
         }
+    }
+
+    [ProtoContract]
+    public class MapData
+    {
+        [ProtoMember(1)]
+        public Vector2 PlayerPosition;
+        [ProtoMember(2)]
+        public float PlayerRotation;
+
+        [ProtoMember(3)]
+        public List<BaseWall> Walls;
+    }
+
+    [ProtoContract]
+    public struct Vector2Surrogate
+    {
+        [ProtoMember(1)]
+        public float X { get; set; }
+
+        [ProtoMember(2)]
+        public float Y { get; set; }
+
+        public static implicit operator Vector2(Vector2Surrogate vector) =>
+            new Vector2(vector.X, vector.Y);
+
+        public static implicit operator Vector2Surrogate(Vector2 vector) =>
+            new Vector2Surrogate { X = vector.x, Y = vector.y };
     }
 }
