3eef92c78bb281410b7d3cd897bfb2652d5c774e
John <johnmichaeljimenez.thecmd@gmail.com>
2026-04-17 11:27:20 +0800

    Program setup

--- Program.cs
+++ Program.cs
@@ -7,13 +7,8 @@ public class Program
 {
     public static void Main()
     {
-        Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
-        Raylib.InitWindow(800, 450, "Raylib-cs Letterbox");
-        Raylib.MaximizeWindow();
-        Raylib.SetTargetFPS(60);
-
-        new Game().Run();
-
-        Raylib.CloseWindow();
+        var game = new Game();
+        game.Run();
+        game.End();
     }
 }
