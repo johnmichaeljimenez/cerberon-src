6d1a61483138869d6e8351e548c8320ede7af232
John <johnmichaeljimenez.thecmd@gmail.com>
2026-04-29 12:14:36 +0800

    Added filter system

--- Core/RenderingManager.cs
+++ Core/RenderingManager.cs
@@ -4,6 +4,31 @@ namespace Main.Core;
 
 public class RenderingManager
 {
+    public class RendererFilter
+    {
+        public string ShaderLocationString;
+        public int ShaderLocation;
+        public float CurrentValue;
+        public float TargetValue;
+
+        public RendererFilter(string locString)
+        {
+            ShaderLocationString = locString;
+        }
+
+        public void Use(Shader shader)
+        {
+            CurrentValue = Raymath.Lerp(CurrentValue, TargetValue, Time.DeltaTime * 20);
+            Raylib.SetShaderValue(shader, ShaderLocation, CurrentValue, ShaderUniformDataType.Float);
+        }
+    }
+
+    public enum Filters
+    {
+        None,
+        Nightvision
+    }
+
     public const int VIRTUAL_WIDTH = 800; //hardcoded for now, might be actual 1080p by default (or at least 720p)
     public const int VIRTUAL_HEIGHT = 450;
 
@@ -23,6 +48,11 @@ public class RenderingManager
     public static float Scale;
     public static Vector2 Offset;
 
+    private static readonly Dictionary<Filters, RendererFilter> AllFilters = new()
+    {
+        { Filters.Nightvision, new("nightAmt") }
+    };
+
     public static void Init()
     {
         SpriteMasked = Raylib.LoadShader(null, "Assets/Shaders/sprite-masked.fs");
@@ -35,7 +65,7 @@ public class RenderingManager
         ReloadShader(AssetWatcher.Add(POST_FX, ReloadShader));
     }
 
-    public static void BeginMaskedShader() //TODO: add a proper sprite rendering system
+    public static void BeginMaskedShader()
     {
         Raylib.BeginShaderMode(SpriteMasked);
         Raylib.SetShaderValueTexture(SpriteMasked, maskVisionTexLoc, LightingSystem.VisionRenderTexture.Texture);
@@ -46,7 +76,7 @@ public class RenderingManager
         var tiling = new Vector2(
                 size.X * Sprite.PIXELS_PER_UNIT / (float)sprite.Texture.Width,
                 size.Y * Sprite.PIXELS_PER_UNIT / (float)sprite.Texture.Height);
-                
+
         Raylib.BeginShaderMode(SpriteTiled);
         Raylib.SetShaderValue(SpriteTiled, tiledTexLocX, tiling.X, ShaderUniformDataType.Float); //TODO: use vector2 (for some reason it doesnt work on that)
         Raylib.SetShaderValue(SpriteTiled, tiledTexLocY, tiling.Y, ShaderUniformDataType.Float);
@@ -64,6 +94,11 @@ public class RenderingManager
         lightTexLoc = Raylib.GetShaderLocation(PostShader, "lightTex");
         visionTexLoc = Raylib.GetShaderLocation(PostShader, "visionTex");
         timeLoc = Raylib.GetShaderLocation(PostShader, "time");
+
+        foreach (var i in AllFilters)
+        {
+            i.Value.ShaderLocation = Raylib.GetShaderLocation(PostShader, i.Value.ShaderLocationString);
+        }
     }
 
     public static void UnloadPostShader()
@@ -93,7 +128,7 @@ public class RenderingManager
     {
         var scaledPos = pos * Scale;
         var scaledSize = size * Scale;
-        
+
         scaledPos += Offset;
         // scaledPos = new Vector2(MathF.Round(scaledPos.X), MathF.Round(scaledPos.Y));
         // scaledSize = new Vector2(MathF.Round(scaledSize.X), MathF.Round(scaledSize.Y));
@@ -116,6 +151,11 @@ public class RenderingManager
             Raylib.SetShaderValueTexture(PostShader, lightTexLoc, LightingSystem.LightingRenderTexture.Texture);
             Raylib.SetShaderValueTexture(PostShader, visionTexLoc, LightingSystem.VisionRenderTexture.Texture);
             Raylib.SetShaderValue(PostShader, timeLoc, Time.CurrentTime, ShaderUniformDataType.Float);
+
+            foreach (var i in AllFilters)
+            {
+                i.Value.Use(PostShader);
+            }
         }
 
         Rectangle source = new(0, 0, target.Texture.Width, -target.Texture.Height);
@@ -124,5 +164,10 @@ public class RenderingManager
 
         if (PostShader.Id != 0)
             Raylib.EndShaderMode();
+    }
+
+    public static void SetFilter(Filters filters, bool enabled)
+    {
+        AllFilters[filters].TargetValue = enabled ? 1 : 0;
     }
 }
