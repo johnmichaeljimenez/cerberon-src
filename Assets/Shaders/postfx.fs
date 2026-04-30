0a4bbf5e6cd3b7eea112d071c86f208e37a9bae5
John <johnmichaeljimenez.thecmd@gmail.com>
2026-05-01 00:36:43 +0800

    Added fade to black (crushing effect)

--- Assets/Shaders/postfx.fs
+++ Assets/Shaders/postfx.fs
@@ -1,18 +1,17 @@
 #version 330
 
 in vec2 fragTexCoord;
-in vec4 fragColor;
 
 uniform sampler2D texture0;
 uniform sampler2D lightTex;
 uniform sampler2D visionTex;
 uniform float time;
 
+uniform float fadeAmt = 0.0; //0 = none, 1 = full
 uniform float nightAmt = 0.0;
 
 const float blurRadius = 1.5;
 
-uniform vec4 colDiffuse;
 out vec4 finalColor;
 
 const int bayer4x4[16] = int[16](
@@ -42,13 +41,23 @@ vec3 contrast(vec3 col, float factor)
     return (col - 0.5) * factor + 0.5;
 }
 
+float luminance(vec3 c)
+{
+    return dot(c, vec3(0.299, 0.587, 0.114));
+}
+
+vec3 fade(vec3 col, float n)
+{
+    return max(vec3(0.0), col - n * 4.5); //simple fade (mix to black) is not enough, this one is the long-lost art common in retro consoles
+}
+
 vec3 nightVision(vec3 tex, vec3 light)
 {
     vec3 col = tex;
     col = contrast(col,0.9);
     col = col + (light * 2);
 
-    const vec3 nightTint = vec3(0.1, 1.0, 0.2);
+    const vec3 nightTint = vec3(0.1, 1.0, 0.2); //I used green phosphor here instead of white phosphor. so red and dark red colors are hard to see like IRL
     col *= nightTint;
 
     float grain = fract(
@@ -97,7 +106,7 @@ void main() {
     lightBlur = contrast(lightBlur, 5); //bloom
 	
     vec3 texelColor = lightColor * (screenColor + (screenColor * lightColor * 2));
-    float lum = pow(dot(blurColor, vec3(0.299, 0.587, 0.114)), 3);
+    float lum = pow(luminance(blurColor), 2.5);
     vec3 screenGrayColor = vec3(lum, lum, lum);
 	
     vec3 nightCol = nightVision(screenColor, lightColor);         
@@ -106,6 +115,9 @@ void main() {
 	float dither = ditherOffset(fragTexCoord);
     finColor += lightBlur * 0.1;
 	finColor += dither; //remove color banding
-	
-	finalColor = vec4(finColor * vig, 1);
+
+    finColor = fade(finColor, fadeAmt);
+    finColor *= vig;
+
+	finalColor = vec4(finColor, 1);
 }
