2e64d758527dbc2e7c1de7f72b2c0effff20d64a
John <johnmichaeljimenez.thecmd@gmail.com>
2026-05-04 13:09:22 +0800

    Added GameplayEventManager

---
+++ Gameplay/Managers/GameplayEventManager.cs
@@ -0,0 +1,69 @@
+using System.Text;
+using Main.Core;
+using Main.Gameplay.Events;
+
+namespace Main.Gameplay.Managers;
+
+public class GameplayEventManager : BaseManager
+{
+	private readonly List<Sequence> sequences = new();
+
+	public GameplayEventManager(GameplayState gameplayState) : base(gameplayState)
+	{
+
+	}
+
+	public override void Update(float dt, float udt)
+	{
+		base.Update(dt, udt);
+
+		if (PauseHandler.IsPaused || sequences.Count == 0)
+			return;
+
+		for (int i = sequences.Count - 1; i >= 0; i--)
+		{
+			var sequence = sequences[i];
+			if (!sequence.IsRunning)
+				sequences.RemoveAt(i);
+			else
+				sequence.Update(dt);
+		}
+	}
+
+	public Sequence RunEvent(string id, params BaseCommand[] commands)
+	{
+		if (commands.Length == 0)
+			throw new InvalidDataException($"Empty command list for: {id}");
+
+		var sequence = new Sequence(gameplayState, id, commands);
+		sequences.Add(sequence);
+
+		return sequence;
+	}
+
+	public override void DrawImGui()
+	{
+		base.DrawImGui();
+
+		if (ImGui.Button("Test"))
+		{
+			RunEvent("test", 
+				new Print("Test"),
+				new Fade(true),
+				new Wait(1.0f),
+				new Print("5"),
+				new Wait(1.0f),
+				new Print("4"),
+				new Wait(1.0f),
+				new Print("3"),
+				new Wait(1.0f),
+				new Print("2"),
+				new Wait(1.0f),
+				new Print("1"),
+				new Wait(1.0f),
+				new Fade(false),
+				new Print("OK!")
+			);
+		}
+	}
+}
