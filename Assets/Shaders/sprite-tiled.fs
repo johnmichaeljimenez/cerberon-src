e8e1c501c6f42a2109db827f02c3d31561fbb94e
John <johnmichaeljimenez.thecmd@gmail.com>
2026-05-13 23:39:26 +0800

    Added stochastic tiling

--- Assets/Shaders/sprite-tiled.fs
+++ Assets/Shaders/sprite-tiled.fs
@@ -7,13 +7,45 @@ uniform sampler2D texture0;
 uniform float tilingX;
 uniform float tilingY;
 
+uniform bool stochasticMode = false;
+
 out vec4 finalColor;
 
 void main()
 {
-	vec2 t = vec2(tilingX, tilingY);
-    vec2 uv = fract(fragTexCoord * t); //tile this regardless of wrap mode from outside
-    vec4 texel = texture(texture0, uv);
-	
+    vec2 t = vec2(tilingX, tilingY);
+    vec2 base_uv = fragTexCoord * t;
+    vec2 local_uv = fract(base_uv);    
+    vec2 tile     = floor(base_uv);    
+
+    vec4 texel;
+
+    if (stochasticMode) //for noisy or organic tiled textures
+    {
+        float hash   = fract(sin(dot(tile, vec2(12.9898, 78.233))) * 43758.5453);
+        float hash2  = fract(hash * 17.313);   
+
+        int   rot    = int(hash * 4.0);      
+        bool  flipH  = hash2 > 0.5;          
+
+        vec2 suv = local_uv;
+
+        if (flipH)
+            suv.x = 1.0 - suv.x;
+
+        if (rot == 1)    
+            suv = vec2(suv.y, 1.0 - suv.x);
+        else if (rot == 2) 
+            suv = vec2(1.0 - suv.x, 1.0 - suv.y);
+        else if (rot == 3) 
+            suv = vec2(1.0 - suv.y, suv.x);
+
+        texel = texture(texture0, suv);
+    }
+    else
+    {
+        texel = texture(texture0, local_uv);
+    }
+
     finalColor = texel * fragColor;
 }
