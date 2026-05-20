e92ecf0d55c70e62de0caea730a709a41cb0cbf7
John <johnmichaeljimenez.thecmd@gmail.com>
2026-05-21 01:35:54 +0800

    Added DoorEntity

---
+++ Gameplay/Entities/DoorEntity.cs
@@ -0,0 +1,79 @@
+using System.ComponentModel;
+using Main.Core;
+using Main.Effects;
+using Main.Gameplay.Level;
+using Main.Gameplay.Managers;
+
+namespace Main.Gameplay.Entities;
+
+public class DoorEntity : BaseEntity, IWaypointModifier
+{
+	private readonly Vector2 DEFAULT_SIZE = new Vector2(3, 1);
+
+	[JsonProperty]
+	public float Rotation { get; set; }
+
+	[JsonProperty]
+	public Vector2 Size { get; set; }
+
+	[JsonIgnore]
+	public bool Interactable { get; set; }
+
+	[JsonIgnore]
+	public bool IsOpen { get; set; }
+
+	[JsonIgnore]
+	public WaypointManager.Node Node { get; set; }
+
+	[JsonIgnore]
+	public List<Wall> Colliders { get; set; } = new();
+	private Shadow shadow;
+
+	public override void Init(GameplayState gameplayState)
+	{
+		base.Init(gameplayState);
+
+		var size = Size;
+		if (MathF.Abs(size.X) <= 0.01f)
+			size.X = DEFAULT_SIZE.X;
+		if (MathF.Abs(size.Y) <= 0.01f)
+			size.Y = DEFAULT_SIZE.Y;
+
+		Size = size;
+
+		gameplayState.GetManager<CollisionManager>().AddWalls(Position, Size, Colliders, Wall.WallFlags.DrawOverlay, CollisionHeight.High, false, Rotation);
+		shadow = LightingSystem.AddShadow(Position, Size, Rotation);
+	}
+
+	public override void Dispose()
+	{
+		base.Dispose();
+
+		Colliders.ForEach(p => gameplayState.GetManager<CollisionManager>().RemoveWall(p));
+		if (shadow != null)
+			LightingSystem.RemoveShadow(shadow);
+	}
+
+	protected override void OnActiveStateChanged(bool isActive)
+	{
+		base.OnActiveStateChanged(isActive);
+		SetState();
+	}
+
+	public void SetOpen(bool isOpen)
+	{
+		IsOpen = isOpen;
+		SetState();
+	}
+
+	private void SetState()
+	{
+		Node.Enabled = IsOpen || !IsActive;
+
+		shadow.Enabled = !IsOpen && IsActive;
+		foreach (var i in Colliders)
+		{
+			i.Enabled = !IsOpen && IsActive;
+		}
+	}
+}
