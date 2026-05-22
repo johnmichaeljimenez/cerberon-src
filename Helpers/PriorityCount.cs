fc8a09199c6517195e5103c8e482c7d26ca402da
John <johnmichaeljimenez.thecmd@gmail.com>
2026-05-22 23:10:32 +0800

    Added priority count system

---
+++ Helpers/PriorityCount.cs
@@ -0,0 +1,36 @@
+namespace Main.Helpers;
+
+public class PriorityCount
+{
+	public int Current { get; private set; }
+	private readonly Dictionary<string, int> activeIDs = new();
+
+	public Action OnChanged;
+
+	public void Add(string id, int value)
+	{
+		activeIDs[id] = value;	//allow updating if it exists
+		Update();
+	}
+
+	public void Remove(string id)
+	{
+		if (!activeIDs.ContainsKey(id))
+			return;
+
+		activeIDs.Remove(id);
+		Update();
+	}
+
+	public void Clear()
+	{
+		activeIDs.Clear();
+		Update();
+	}
+
+	private void Update()
+	{
+		Current = activeIDs.Count > 0? activeIDs.Max(p => p.Value) : 0;	//TODO: optimize if needed
+		OnChanged?.Invoke();
+	}
+}
