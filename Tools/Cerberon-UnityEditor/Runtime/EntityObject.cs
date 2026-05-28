558abdfdc4a9b759c742f8cb6490af81ffa0f387
John <johnmichaeljimenez.thecmd@gmail.com>
2026-05-29 01:52:56 +0800

    Added EntityObject component

---
+++ Tools/Cerberon-UnityEditor/Runtime/EntityObject.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class EntityObject : MonoBehaviour
+{
+	public string NameTag = "";
+	public string EntityType = "Item";
+
+	//TEMPORARY
+	[TextArea]
+	public string Properties;
+}
