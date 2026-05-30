0f31b5a26798c4de932da6aec0e92e12dd941cc3
John <johnmichaeljimenez.thecmd@gmail.com>
2026-05-30 15:53:46 +0800

    Added main menu logo

--- UI/MainMenuScreen.cs
+++ UI/MainMenuScreen.cs
@@ -7,15 +7,35 @@ public class MainMenuScreen : BaseScreen
 {
 	public override string UIGroup => "MainMenu";
 
+	private Sprite logoDefault, logoGlow;
 
 	public MainMenuScreen(object context) : base(context)
 	{
-
+		logoDefault = AssetManager.GetSprite("ui/logo");
+		logoGlow = AssetManager.GetSprite("ui/logo-glow");
 	}
 
 	public override void OnBack()
 	{
-		
+
+	}
+
+	public override void UpdateElements(List<UIElement> elements)
+	{
+		base.UpdateElements(elements);
+
+		var title = references["title"];
+		title.CustomDrawing = (e) =>
+		{
+			var flicker = QuakeFlicker.GetIntensity();
+			var sprite = flicker >= 0.8f ? logoGlow : logoDefault;
+			var r = e.GetAspectRatioRectangle(new(sprite.Width, sprite.Height));
+
+			Raylib.DrawTexturePro(sprite.Texture,
+				new Rectangle(0, 0, sprite.Width, sprite.Height),
+				r,
+				Vector2.Zero, 0, Color.White);
+		};
 	}
 
 	protected override void OnClick(UIElement e)
