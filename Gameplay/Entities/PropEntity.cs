9addd95a14fc9b0ee27f13318d957fa5bbf1376b
John <johnmichaeljimenez.thecmd@gmail.com>
2026-06-23 12:09:25 +0800

    Added PropEntity

---
+++ Gameplay/Entities/PropEntity.cs
@@ -0,0 +1,73 @@
+using Cerberon.Core;
+using Cerberon.Effects;
+using Cerberon.Gameplay.Level;
+using Cerberon.Gameplay.Managers;
+using Cerberon.Helpers;
+using static Cerberon.Gameplay.Managers.WaypointManager;
+
+namespace Cerberon.Gameplay.Entities;
+
+public class PropEntity : BaseEntity
+{
+	[JsonProperty]
+	public float Rotation { get; set; }
+
+	[JsonProperty]
+	public Vector2 ColliderSize { get; set; }
+
+	[JsonIgnore]
+	public List<Wall> Colliders { get; set; } = new();
+
+	private readonly List<Node> nodes = new();
+
+	public override void Init(GameplayState gameplayState)
+	{
+		base.Init(gameplayState);
+
+		gameplayState.GetManager<CollisionManager>().AddWalls(Position, ColliderSize, Colliders, Wall.WallFlags.DrawOverlay, CollisionHeight.Low, false, Rotation);
+
+		foreach (var i in Colliders)
+		{
+			i.Entity = this;
+			i.Enabled = IsActive;
+		}
+	}
+
+	public override void PostInit()
+	{
+		base.PostInit();
+
+		nodes.AddRange(gameplayState.GetManager<WaypointManager>().GetNodesInsideRect(Position, ColliderSize, Rotation));
+		foreach (var i in nodes)
+		{
+			i.Enabled = !IsActive;
+		}
+	}
+
+	protected override void OnActiveStateChanged(bool isActive)
+	{
+		base.OnActiveStateChanged(isActive);
+		
+		foreach (var i in nodes)
+		{
+			i.Enabled = !IsActive;
+		}
+
+		foreach (var i in Colliders)
+		{
+			i.Enabled = IsActive;
+		}
+	}
+
+	public override void Dispose()
+	{
+		base.Dispose();
+
+		Colliders.ForEach(p => gameplayState.GetManager<CollisionManager>().RemoveWall(p));
+	}
+
+	public override void Draw()
+	{
+		CurrentSprite?.Draw(Position, 1, Rotation);
+	}
+}
