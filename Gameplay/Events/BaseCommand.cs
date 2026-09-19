b867b4f44b45fbd3fe07284080759db5a16cd78f
John <johnmichaeljimenez.thecmd@gmail.com>
2026-09-19 23:57:03 +0800

    Added SetTriggerActive

--- Gameplay/Events/BaseCommand.cs
+++ Gameplay/Events/BaseCommand.cs
@@ -172,3 +172,18 @@ public class SetActive : GameCommand
 		gameplayState.CurrentWorld.GetEntityByNameTag<BaseEntity>(nameTag).IsActive = isActive;
 	}
 }
+
+public class SetTriggerActive : GameCommand
+{
+	[CommandParameter]
+	private string id;
+
+	[CommandParameter]
+	private bool isActive = true;
+
+	public override void OnEnter()
+	{
+		base.OnEnter();
+		gameplayState.GetManager<TriggerManager>().Find(id).ForEach(p => p.Enabled = isActive);
+	}
+}
